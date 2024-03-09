e78205063852f9a9ea702541fc51708dda0d7158
Roland VM <dasycarpum@gmail.com>
2024-03-09 16:47:47 +0100

    create main function

---
+++ src/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.11
+# -*- coding: utf-8 -*-
+"""
+Created on 2024-03-09
+
+@author: Roland VANDE MAELE
+
+@abstract: the entry point of the program.
+
+"""
+
+
+def main():
+    """
+    Main script entry point.
+
+    This function serves as the entry point for the script. It takes no 
+    arguments, returns nothing. It is intended to be called when the script is 
+    executed directly.
+
+    Args:
+        None.
+
+    Returns:
+        None.
+    
+    Raises:
+        No exception is raised by this function.
+    """
+
+    print("test")
+
+
+if __name__ == "__main__":
+    main()
