7450568f1ac39102e396fae960c026e624fe8156
Roland VM <dasycarpum@gmail.com>
2024-03-09 21:52:14 +0100

    set up and manage the database connection

---
+++ src/models/base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.11
+# -*- coding: utf-8 -*-
+"""
+Created on 2024-03-09
+
+@author: Roland VANDE MAELE
+
+@abstract: this file is responsible for setting up and managing the database connection. 
+
+"""
+
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URI = (
+    f"postgresql+psycopg2://"
+    f"{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/"
+    f"{os.getenv('DB_NAME')}"
+)
+
+engine = create_engine(DATABASE_URI)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
