0eb6b4f987c9f60002878b3aca7cda4e35c259a1
Roland VM <dasycarpum@gmail.com>
2024-03-14 20:39:06 +0100

    extract stock data needed for clustering company profiles

---
+++ src/dal/fmp/database_query.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3.11
+# -*- coding: utf-8 -*-
+"""
+Created on 2024-03-14
+
+@author: Roland VANDE MAELE
+
+@abstract: the class act as an interface between the application and the 
+database, selecting data into the database via SQLAlchemy. These functions are 
+part of the data access logic (DAL).
+
+"""
+
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+from src.models.base import Session
+
+class StockQuery:
+    """
+    A class for performing query operations related to stock data in the database.
+
+    This class provides functionality to query stock data, specifically designed
+    for operations such as extracting data necessary for clustering company
+    profiles based on certain financial metrics.
+
+    """
+
+    def __init__(self, db_session: Session):
+        """
+        Initializes the StockQuery with a database session.
+
+        Args:
+            db_session (Session): The database session used for executing
+                                  queries against the database.
+        """
+        self.db_session = db_session
+
+    def extract_data_for_company_profiles_clustering(self):
+        """
+        Extracts stock data needed for clustering company profiles.
+
+        This method queries the database for specific attributes of company
+        profiles: stock_id, beta, vol_avg, and mkt_cap. These attributes are
+        essential for performing clustering analysis to identify groups of
+        companies with similar trading characteristics.
+
+        Returns:
+            A list of tuples where each tuple contains the stock_id, beta,
+            vol_avg, and mkt_cap of a company, representing the data needed
+            for clustering analysis.
+
+        Raises:
+            SQLAlchemyError: An error occurred during the database transaction.
+                             The transaction is rolled back and the session
+                             is closed in case of an error.
+        """
+        try:
+            query = text(
+                "SELECT stock_id, beta, vol_avg, mkt_cap FROM companyprofile")
+            data = self.db_session.execute(query).fetchall()
+
+            return data
+
+        except SQLAlchemyError as e:
+            # Rollback the transaction in case of an error
+            self.db_session.rollback()
+            print(f"An error occurred: {e}")
+
+        finally:
+            # Close session in all cases (on success or failure)
+            self.db_session.close()
--- src/main.py
+++ src/main.py
@@ -9,11 +9,9 @@ Created on 2024-03-09
 
 """
 
-from src.models.base import engine, Session, Base
+from src.models.base import Session
 from src.models.fmp.stock import StockSymbol, CompanyProfile, DailyChartEOD # pylint: disable=unused-import
-from src.business_logic.fmp.database_process import StockService
-from src.services.sql import convert_stock_table_to_hypertable
-
+from src.dal.fmp.database_query import StockQuery
 
 def main():
     """
@@ -32,30 +30,12 @@ def main():
     Raises:
         No exception is raised by this function.
     """
-
-    # Creating tables for imported models
-    Base.metadata.create_all(engine)
-
-    # Converting in hypertable (TimeScaleDB)
-    hypertable_candidates = ['dailychart', 'dividend', 'keymetrics']
-    for table in hypertable_candidates:
-        convert_stock_table_to_hypertable(table)
-
-    # Completing tables with FMP data
     db_session = Session()
-    stock_service = StockService(db_session)
+    stock_query = StockQuery(db_session)
 
-    # stock_service.fetch_stock_symbols()
-    european_exchanges = ["Copenhagen", "Madrid Stock Exchange", "Frankfurt Stock Exchange", "Paris", "Irish", "Lisbon", "Brussels", "Milan", "Amsterdam", "Helsinki", "Vienna", "Stockholm Stock Exchange", "Swiss Exchange", "London Stock Exchange", "Oslo Stock Exchange", "Athens", "Prague", "Warsaw Stock Exchange", "Riga", "Budapest", "Iceland"]
-    for exchange in european_exchanges:
-        stock_service.fetch_company_profiles_for_exchange(exchange)
+    data = stock_query.extract_data_for_company_profiles_clustering()
 
-    symbols = ['AAPL', 'MSFT']
-    for symbol in symbols:
-        # stock_service.fetch_daily_chart_for_period(symbol, "1990-01-01", "1995-01-01")
-        # stock_service.fetch_historical_dividend(symbol)
-        stock_service.fetch_historical_key_metrics(symbol)
-
+    print(data[:5])
 
 if __name__ == "__main__":
     main()
