c1f005261a0e73e74f2d3b477651a9954098fcca
Roland VM <dasycarpum@gmail.com>
2024-03-18 15:06:50 +0100

    convert sxxp into hypertable and complete it with STOXX data

--- src/main.py
+++ src/main.py
@@ -9,9 +9,10 @@ Created on 2024-03-09
 
 """
 
-from src.models.base import Session
-from src.models.fmp.stock import StockSymbol, CompanyProfile, DailyChartEOD # pylint: disable=unused-import
-from src.dal.fmp.database_query import StockQuery
+from src.models.base import engine, Session, Base
+from src.models.fmp.stock import StockSymbol, CompanyProfile, STOXXEurope600 # pylint: disable=unused-import
+from src.business_logic.fmp.database_process import StockService
+from src.services.sql import convert_stock_table_to_hypertable
 
 
 def main():
@@ -31,9 +32,17 @@ def main():
     Raises:
         No exception is raised by this function.
     """
-    db_session = Session()
-    stock_query = StockQuery(db_session)
 
+    # Creating tables for imported models
+    Base.metadata.create_all(engine)
+
+    # Hypertable (TimeScaleDB)
+    convert_stock_table_to_hypertable('sxxp')
+
+    db_session = Session()
+    stock_service = StockService(db_session)
+
+    stock_service.fetch_sxxp_historical_components('20240301')
 
 
 if __name__ == "__main__":
